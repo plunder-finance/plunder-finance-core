4341798a184ba882c488138adc7f4338b237a4a8
l0ngj0hns1lver <l0ngj0hns1lver@protonmail.com>
2022-04-28 23:39:21 +0300

    add one strategy to the repo

---
+++ scripts/deploy-all.js
@@ -0,0 +1,46 @@
+async function main() {
+  const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
+
+  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
+  const tokenName = 'FTM-APE Spooky Crypt';
+  const tokenSymbol = 'rf-FTM-APE';
+  const depositFee = 0;
+  const tvlCap = ethers.constants.MaxUint256;
+
+  const vault = await Vault.deploy(wantAddress, tokenName, tokenSymbol, depositFee, tvlCap);
+
+  await vault.deployed();
+  console.log('Vault deployed to:', vault.address);
+
+  const vaultAddress = '0xb1f916892c649EB163328F952642199F106C08b4';
+
+  const Strategy = await ethers.getContractFactory('ReaperStrategySpooky');
+  const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
+  const paymentSplitterAddress = '0x63cbd4134c2253041F370472c130e92daE4Ff174';
+  const strategist1 = '0x1E71AEE6081f62053123140aacC7a06021D77348';
+  const strategist2 = '0x81876677843D00a7D792E1617459aC2E93202576';
+  const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
+  const poolId = 70;
+
+  const strategy = await hre.upgrades.deployProxy(
+    Strategy,
+    [
+      vaultAddress,
+      [treasuryAddress, paymentSplitterAddress],
+      [strategist1, strategist2, strategist3],
+      wantAddress,
+      poolId,
+    ],
+    {kind: 'uups', timeout: 0},
+  );
+
+  await strategy.deployed();
+  console.log('Strategy deployed to:', strategy.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
