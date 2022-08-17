251891cb086cd02010cbb9ee2ad2b86fa34291a4
l0ngj0hns1lver <l0ngj0hns1lver@protonmail.com>
2022-08-17 17:08:57 +0300

    add deploy scripts

---
+++ scripts/deploy/deploy-and-use-zksync.js
@@ -0,0 +1,26 @@
+// @ts-nocheck
+const { artifacts, web3, accounts, network } = require('hardhat')
+const { ether, time, expectRevert } = require('@openzeppelin/test-helpers')
+const { ADDRESSES: ALL_ADDRESSES, impersonateAccount, getDeployAddressAfter } = require('./../../test/fork/common')
+
+const IUniswapV2Router02 = artifacts.require('contracts/BIFI/interfaces/common/IUniswapRouterETH.sol:IUniswapRouterETH')
+const PlunderVault = artifacts.require('PlunderVaultV6')
+const PlunderFinanceTreasury = artifacts.require('PlunderTreasury')
+const IUniV2Pair = artifacts.require('contracts/BIFI/interfaces/common/IUniswapV2Pair.sol:IUniswapV2Pair')
+// const StrategyTriMiniChefLP = artifacts.require('StrategyTriMiniChefLP')
+const IERC20 = artifacts.require('@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20')
+const StrategyTriMiniChefDualLP = artifacts.require('StrategyTriMiniChefDualLP')
+const IERC20Extended = artifacts.require('IERC20Extended')
+
+
+async function main() {
+
+}
+
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
